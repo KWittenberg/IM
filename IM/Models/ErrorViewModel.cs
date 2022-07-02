021cf51a787759b64e1d643ea9a0095ae99e0f9f
K. Wittenberg <kejo@net.hr>
2022-07-02 13:36:13 +0200

    Add project files.

---
+++ IM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
